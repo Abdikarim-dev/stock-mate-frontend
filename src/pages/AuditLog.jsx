02d1635210587f0378e2f6472827cf01f82b0744
Abdikarim-dev <cabdikariim405@gmail.com>
2025-12-13 09:13:54 +0300

    Audit Log Page

---
+++ src/pages/AuditLog.jsx
@@ -0,0 +1,17 @@
+import AuditLogTable from "../components/audit/AuditLogTable";
+
+const AuditLog = () => {
+  
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <span className="text-3xl font-bold tracking-tight">Logs</span>
+      </div>
+      {/* <div>User Table View</div> */}
+
+      <AuditLogTable />
+    </div>
+  );
+};
+
+export default AuditLog;
